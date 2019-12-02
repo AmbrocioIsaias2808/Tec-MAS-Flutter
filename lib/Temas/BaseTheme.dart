3e3f5801560b410579fd8990d4d8947cc9dc7188
AmbrocioIsaias2808 <44043979+AmbrocioIsaias2808@users.noreply.github.com>
2019-12-02 10:16:45 -0600

    d

--- lib/Temas/BaseTheme.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color BaseThemeColor_DarkBlue = Color.fromRGBO(27, 55, 94,1);
-Color BaseThemeColor_DarkLightBlue = Color.fromRGBO(1, 94, 193,1);
-Color BaseThemeColor_LightGray= Color.fromRGBO(220,200,220,0.8);
